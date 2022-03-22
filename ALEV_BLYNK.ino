552b4925c013448e5449c8b0127af25425ae70da
Betül Büyükzengin <62355935+BetulBuyukzengin@users.noreply.github.com>
2022-03-22 21:50:14 +0300

    Add files via upload

---
+++ ALEV_BLYNK.ino
@@ -0,0 +1,66 @@
+// ALEV SENSORU İLE ODADAKI ALEV ALGILANINCA ALARMIN ÇALIŞMASI,TELEFONA BİLDİRİMİN GELMESİ 
+
+//Gerekli kütüphanelerın tanımlanması
+#include <ESP8266WiFi.h> // nodemcu esp8266 wifi modulu kutuphanesı
+#include <BlynkSimpleEsp8266.h> // blynk uygulaması için kütüphane
+#define BLYNK_PRINT Serial  
+/*************************************************************************************************************************************/
+
+char auth[] = "wYUB5t1DbhHKflhvzEmR1MezZwCK5vno"; //blynk uygulamasının tokenı 
+
+char ssid[] = "Xiaomi_DA23"; //baglanılan wifi ssid 
+
+char pass[] = "Y3VW7EXWNAFT"; //wifi sifre
+/*************************************************************************************************************************************/
+
+int sensorPin = D2; // sensorPin degiskeni D2 pinine ayarlandı
+int sensorDeger;
+int alevbuzzer = D3; //alevbuzzer D3 pini olarak ayarlandı 
+bool bildirim = true; //bool turudeki bildirim degiskeni true olarak ayarlandı
+BlynkTimer timer;// timer adında bir nesne oluşturuldu.
+
+/*************************************************************************************************************************************/
+
+//Sensörün verdiği değerleri okuyup verileri uygulamaya gönderiyor.
+void sensorOku()
+{
+  sensorDeger = analogRead(sensorPin); // sensorPin den okunan deger sensorDeger e atanıyor.
+
+  //Sensör verisi 300 ün altına düştüğünde ve bildirim değişkeni true oldugunda döngü çalışır.
+  if (sensorDeger < 300 && bildirim) {
+    Blynk.notify("DİKKAT! YANGIN VAR! "); //uygulamadan "DİKKAT! YANGIN VAR! " şeklinde  uyarı mesajı alınacak
+
+    //for döngüsünde D3 pinine bağlı buzzerın 5 kere uyarı vermesİ saglanıyor.
+    for (int i = 0; i < 5; i++) {
+      digitalWrite(alevbuzzer, HIGH);
+      delay(300);
+      digitalWrite(alevbuzzer, LOW);
+      delay(300);
+    }
+    bildirim = false;
+  }
+
+  //sensorDeger 700 ün üzerinde çıktığında döngü çalışır.
+  if (sensorDeger > 700) {
+    bildirim = true;
+  }
+}
+
+ /*************************************************************************************************************************************/
+
+void setup()//setup içine 1 kez girilir.
+{
+  Serial.begin(9600);////Serial Monitor 9600 bound olarak baslatıldı
+  Blynk.begin(auth, ssid, pass);// yukarıda verilen auth ,ssid,pass degerleri sırasıyla gelecek.
+  Blynk.notify("Cihaz Açıldı"); // Başlangıçta Cihaz Açıldı bildirimi gonderılıyor.
+  pinMode(alevbuzzer, OUTPUT); // alevbuzzer pini cıkıs olarak ayarlandı
+ 
+  timer.setInterval(1000L, sensorOku); // Saniyede 1 defa sensorOku fonksiyonu çağırılacak
+}
+ /*************************************************************************************************************************************/
+
+void loop()
+{
+  timer.run(); //timer calısacak
+  Blynk.run(); // tum etkinlikler blynk uygulaması ile çalısacak 
+}
